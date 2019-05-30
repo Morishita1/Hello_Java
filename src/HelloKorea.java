144faf213099c9656ab48f152b1abb1a92a0bacb
Morishita1 <iakwpi@naver.com>
2019-05-30 14:25:23 +0900

    first

---
+++ src/HelloKorea.java
@@ -0,0 +1,37 @@
+
+// 지금부터  helloKorea 소스코드를 작성하겠습니다
+// 작성일자 : 2019-05-29
+// 작성자 : iakwpi@naver.com
+// 용도 : 자바 소스코드 연습 시작
+public class HelloKorea {
+
+	// method
+	public static void main(String[] args) {
+		
+		System.out.println("Republuc of Korea");
+		
+		System.out.println("우리나라만세");
+		System.out.println(30+40);
+		System.out.println("30+40");
+		System.out.println(30*40);
+		System.out.println(30-40);
+		System.out.println(30/40);
+		System.out.println(30+40);
+		System.out.println(40/20);
+		System.out.println(30%40);
+		
+		System.out.println(1%2);
+		System.out.println(2%2);
+		System.out.println(3%2);
+		System.out.println(4%2);
+		System.out.println(5%2);
+		System.out.println(6%2);
+		System.out.println(7%2);
+		System.out.println(8%2);
+		System.out.println(9%2);
+		System.out.println(10%2);
+		
+		
+	}
+
+}
